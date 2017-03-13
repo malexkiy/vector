0468b4d8ea7b967ad4352ab963c628a4a7a54c05
malexkiy <malexkiy@gmail.com>
2017-03-13 13:22:13 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -11,42 +11,46 @@ SCENARIO("vector init param", "[init param]") {
 	vector_t vector(4);
 	REQUIRE(vector.size() == 4);
 	REQUIRE(vector.capacity() == 4);
-	REQUIRE(vector[0] == 0);
-	REQUIRE(vector[1] == 0);
-	REQUIRE(vector[2] == 0);
-	REQUIRE(vector[3] == 0);
+	for(unsigned int i = 0; i < vector.size(); i++)
+		vector[i] == 0;
 }
 
 SCENARIO("vector init object", "[init object]") {
 	vector_t b;
+	REQUIRE(b.size() == 0);
+	REQUIRE(b.capacity() == 0);
 
 	b.push_back(1);
 	b.push_back(2);
 	b.push_back(3);
+	REQUIRE(b.size() == 3);
+	REQUIRE(b.capacity() == 4);
 
 	vector_t a(b);
 
 	REQUIRE(a.size() == 3);
 	REQUIRE(a.capacity() == 4);
-	REQUIRE(a[0] == b[0]);
-	REQUIRE(a[1] == b[1]);
-	REQUIRE(a[2] == b[2]);
+	for(unsigned int i = 0; i < a.size(); i++)
+		a[i] == b[i];
 }
 
 SCENARIO("vector op=", "[op=]") {
 	vector_t a(4), b;
+	REQUIRE(b.size() == 0);
+	REQUIRE(b.capacity() == 0);
 
 	b.push_back(1);
 	b.push_back(2);
 	b.push_back(3);
+	REQUIRE(b.size() == 3);
+	REQUIRE(b.capacity() == 4);
 
 	a = b;
 
 	REQUIRE(a.size() == 3);
 	REQUIRE(a.capacity() == 4);
-	REQUIRE(a[0] == b[0]);
-	REQUIRE(a[1] == b[1]);
-	REQUIRE(a[2] == b[2]);
+	for(unsigned int i = 0; i < a.size(); i++)
+		a[i] == b[i];
 }
 
 SCENARIO("vector push", "[push]") {
@@ -54,10 +58,10 @@ SCENARIO("vector push", "[push]") {
 	REQUIRE(vector.size() == 3);
 	REQUIRE(vector.capacity() == 3);
 	vector.push_back(4);
+	REQUIRE(vector.size() == 4);
+	REQUIRE(vector.capacity() == 4);
 	REQUIRE(vector[0] == 0);
 	REQUIRE(vector[1] == 0);
 	REQUIRE(vector[2] == 0);
 	REQUIRE(vector[3] == 4);
-	REQUIRE(vector.size() == 4);
-	REQUIRE(vector.capacity() == 4);
 }
